b5a56fe52eb731839a3c7b0762929d4a2af82592
robin mahmud <robinmahmud358@gmail.com>
2022-08-18 22:07:56 +0600

    case part done

---
+++ script.js
@@ -0,0 +1,31 @@
+function updateCase(increse) {
+    const inputCasing = document.getElementById('case-number-field');
+    const priviousString = inputCasing.value;
+    const priviousValue = parseInt(priviousString);
+    let newValue;
+    if (increse === true) {
+        newValue = priviousValue + 1;
+    }
+    else {
+        newValue = priviousValue - 1;
+    }
+    inputCasing.value = newValue;
+    return newValue;
+
+}
+function updatecaseTotalPrice(totalCase) {
+    const totalCaseAmount = totalCase * 59;
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = totalCaseAmount;
+}
+
+document.getElementById('btn-case-pluse').addEventListener('click', function () {
+    const totalCase = updateCase(true);
+    updatecaseTotalPrice(totalCase);
+})
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    const totalCase = updateCase(false);
+    updatecaseTotalPrice(totalCase);
+
+
+})
